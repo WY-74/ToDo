575e7800196613d34c7d2a2f5851456785cd342c
WangYun <294961525@qq.com>
2023-07-31 21:46:15 +0800

    2307031: update weixin

--- pages/weixin/weixin_pages.py
+++ pages/weixin/weixin_pages.py
@@ -6,14 +6,14 @@ from data.weixin import weixin_location
 class WeixinPages(BaseRequests):
     def __init__(self, driver: None):
         super().__init__(driver)
-        self.yaml = "/Users/ya/wangyun/ToDo/assert/weixin.yaml"
+        self.yaml = "/Users/ya/wangyun/ToDo/mapper/weixin.yaml"
         self.data = weixin_data
         self.location = weixin_location
 
     def get_access_token(self):
         response = self.http_methods("get", self.data.AccessToken.url, params=self.data.AccessToken.params)
         self.assert_by_yamlmap(response, self.yaml)
-        self.get_token(response, self.location.AccessToken.get_token, "access_token")
+        self.get_token(response, self.location.AccessToken.get_token)
 
     def create(self):
         response = self.http_methods("post", self.data.Create.url, params=self.token, json=self.data.Create.json_params)
